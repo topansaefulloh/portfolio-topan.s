e1a327ee2b11c9d1fc49d822ef6077d417069439
topansaefulloh <topansaefulloh123@gmail.com>
2023-01-24 12:46:15 +0700

    update darkmode toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
@@ -14,8 +15,8 @@ module.exports = {
       },
       screens: {
         '2xl': '1320px',
-      }
+      },
     },
   },
   plugins: [],
-}
+};
